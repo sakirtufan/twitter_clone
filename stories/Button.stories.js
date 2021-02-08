c2b27ce4a7be890bccfa04f947053a97cd5b049b
sakir tufan <sakirtufan49@gmail.com>
2021-02-08 19:36:50 +0100

    Create Theme Button

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -4,12 +4,19 @@ import TextTitle from '../components/text-title'
 import Button from "../components/button";
 import NavigationButton from "../components/navigation-button"
 import Navigation from "../components/navigation";
+import ThemeButton from "../components/theme-button";
 
 export default {
   title: "Buttons",
 };
 
 export const Normal = () => <Button>Save</Button>;
+
+export const Theme = () => <div>
+  <ThemeButton>Save</ThemeButton>
+  <ThemeButton full >Save Full</ThemeButton>
+  <ThemeButton full big>Save Big Button</ThemeButton>
+</div>;
 
 export const NavButton = () => (
   <NavigationButton>
