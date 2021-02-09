d815d54688804668ad33fcd60acc66bbe92a0251
sakir tufan <sakirtufan49@gmail.com>
2021-02-09 22:05:47 +0100

    Add Size Property To Photo Component

--- components/photo.js
+++ components/photo.js
@@ -5,9 +5,10 @@ import styles from "./photo.module.css";
 function Photo({
   src = "https://pbs.twimg.com/profile_images/1301602827563937795/F1cWHNzt_400x400.jpg",
   alt,
+  size = 47
 }) {
   return (
-    <div className={cn([styles.photo])}>
+    <div className={cn([styles.photo])} style={{width:size, height:size}}>
       <img className={styles.img} src={src} alt={alt} />
     </div>
   );
