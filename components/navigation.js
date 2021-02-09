446d8255325c482e2bb7b206d70b3549d2684b2d
sakir tufan <sakirtufan49@gmail.com>
2021-02-09 22:07:32 +0100

    Refactor Navigation

--- components/navigation.js
+++ components/navigation.js
@@ -1,55 +1,26 @@
 import React from "react";
-import NavigationButton from "./navigation-button"
-import TextTitle from "./text-title"
-import {Bookmark, Explore, Home, Lists, Messages, More, Notification, Profile, Twitter} from "./icons"
+import NavigationButton from "./navigation-button";
+import TextTitle from "./text-title";
 import styles from "./navigation.module.css";
+import { MENU } from "../constants";
 
-function Navigation({ flat=false,selectedKey }) {
+function Navigation({ flat = false, selectedKey = "home" }) {
   return (
     <nav className={styles.nav}>
-      <NavigationButton>
-        <Twitter/>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "home"}>
-        <Home />
-        <TextTitle>Startseite</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "explore"} >
-        <Explore />
-        <TextTitle>Entdecken</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton notify={17} selected={selectedKey === "notification"}>
-        <Notification />
-        <TextTitle>Mitteilungen</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "messages"}>
-        <Messages />
-        <TextTitle>Nachrichten</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "bookmark"}>
-        <Bookmark />
-        <TextTitle>Lesezeichen</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "lists"}>
-        <Lists />
-        <TextTitle>Listen</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "profile"}>
-        <Profile />
-        <TextTitle>Profil</TextTitle>
-      </NavigationButton>
-
-      <NavigationButton selected={selectedKey === "more"}>
-        <More />
-        <TextTitle>Mehr</TextTitle>
-      </NavigationButton>
+      {MENU.map((menu) => {
+        const showTitle = !flat && menu.title.length > 0;
+        const selected = selectedKey === menu.key
+        return (
+          <NavigationButton
+            key={menu.key}
+            notify={menu.notify}
+            selected={selected}
+          >
+            {selected ? menu.iconSelected : menu.icon}
+            {showTitle && <TextTitle>{menu.title}</TextTitle>}
+          </NavigationButton>
+        );
+      })}
     </nav>
   );
 }
--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,72 @@
+import React from "react";
+import * as Icon from '../components/icons'
 export default {
   TABLET_SIZE: 980,
   DESKTOP_SIZE: 1270
 }
+
+export const MENU = [
+  {
+    key: 'twitter',
+    icon: <Icon.Twitter />,
+    iconSelected: <Icon.Twitter />,
+    title: '',
+    notify: 0
+  },
+  {
+    key: 'home',
+    icon: <Icon.Home />,
+    iconSelected: <Icon.HomeFill />,
+    title: 'Startseite',
+    notify: 0
+  },
+  {
+    key: 'explore',
+    icon: <Icon.Explore />,
+    iconSelected: <Icon.ExplorerFill />,
+    title: 'Entdecken',
+    notify: 0
+  },
+  {
+    key: 'notification',
+    icon: <Icon.Notification />,
+    iconSelected: <Icon.NotificationFill />,
+    title: 'Mitteilungen',
+    notify: 17
+  },
+  {
+    key: 'messages',
+    icon: <Icon.Messages />,
+    iconSelected: <Icon.MessagesFill />,
+    title: 'Nachrichten',
+    notify: 0
+  },
+  {
+    key: 'bookmark',
+    icon: <Icon.Bookmark />,
+    iconSelected: <Icon.BookmarkFill />,
+    title: 'Lesezeichen',
+    notify: 0
+  },
+  {
+    key: 'lists',
+    icon: <Icon.Lists />,
+    iconSelected: <Icon.ListsFill />,
+    title: 'Listen',
+    notify: 0
+  },
+  {
+    key: 'profile',
+    icon: <Icon.Profile />,
+    iconSelected: <Icon.ProfileFill />,
+    title: 'Profil',
+    notify: 0
+  },
+  {
+    key: 'more',
+    icon: <Icon.More />,
+    iconSelected: <Icon.More />,
+    title: 'Mehr',
+    notify: 0
+  }
+]
