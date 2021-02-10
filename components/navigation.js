05c5fe34f021d1f1d8b277b869576970725bcec4
sakir tufan <sakirtufan49@gmail.com>
2021-02-10 08:34:56 +0100

    Refactor Navigation

--- components/navigation.js
+++ components/navigation.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { useRouter } from "next/router";
 import NavigationButton from "./navigation-button";
 import TextTitle from "./text-title";
 import styles from "./navigation.module.css";
 import { MENU } from "../constants";
 
-function Navigation({ flat = false, selectedKey = "home" }) {
+function Navigation({ flat = false }) {
+  const router = useRouter();
+
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
         const showTitle = !flat && menu.title.length > 0;
-        const selected = selectedKey === menu.key
+        const selected = router.pathname === menu.path
         return (
           <NavigationButton
             key={menu.key}
             notify={menu.notify}
             selected={selected}
+            href={menu.path}
+            classnames={styles.navButton}
           >
             {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TextTitle>{menu.title}</TextTitle>}
