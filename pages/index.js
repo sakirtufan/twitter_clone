8251b914b1d022ff822e47203716684d744c81dc
sakir tufan <sakirtufan49@gmail.com>
2021-02-11 18:47:57 +0100

    Add Twit Example To Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 import Layout from "../components/layout";
-
+import Tweet from "../components/tweet";
 
 function HomePage() {
+  return (
+    <Layout>
+      <Tweet
+        name="Sakir Tufan"
+        slug="tufan_sakir"
+        datetime={new Date("2021,02,10")}
+        text={`aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
- return <Layout>Startseite</Layout>
+aaaaaaaaaaaa
+
+                     aaaaaaaaaaaaaaaaaaaaaaaaaa
+              `}
+      >
+        <p>Hallo erste Tweet</p>
+      </Tweet>
+    </Layout>
+  );
 }
 
-export default HomePage
+export default HomePage;
